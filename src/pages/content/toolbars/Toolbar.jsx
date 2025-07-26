091b13be916e4c43f1cd6e3a7d350968c299047b
GabrielSER <gs.serna10@uniandes.edu.co>
2025-07-26 03:42:27 -0500

    Mjolnir fix 2

--- src/pages/content/toolbars/Toolbar.jsx
+++ src/pages/content/toolbars/Toolbar.jsx
@@ -1,7 +1,5 @@
 import { DiceType } from '../dice/Dice'
-import { GiPistolGun } from 'react-icons/gi'
 import Dices from './dices/Dices'
-import FirearmsCalculator from './FirearmsCalculator'
 
 const Toolbar = {
   DICES: {
@@ -10,13 +8,6 @@ const Toolbar = {
     name: 'Dices',
     longName: 'Dice roller',
     icon: DiceType.D20.logo
-  },
-  FIREARMS_CALCULATOR: {
-    id: 'firearms_calculator',
-    component: <FirearmsCalculator />,
-    name: 'Firearms',
-    longName: 'Firearms calculator',
-    icon: <GiPistolGun />
   }
 }
 
